e1cf98a6c00b67fc34d63d71281d737592c99578
sumbul mohammad saleem Jakhura <62174359+sumbuljakhura@users.noreply.github.com>
2024-03-29 03:19:19 +0500

    Add files via upload

---
+++ Q9.js
@@ -0,0 +1,10 @@
+// Q9=Write a simple JavaScript program that checks if a person is 
+// eligible for a CNIC 
+var names = "sumbul";
+var age = 23;
+if (age >= 18) {
+    console.log("Congratulations!" + names + " You are eligible to apply for a CNIC");
+}
+else {
+    console.log("Sorry" + names + " You are not eligible to apply for a CNIC yet.");
+}
